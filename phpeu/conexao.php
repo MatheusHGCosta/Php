71ae766d3d38305c5909e50dd7d554de1e840612
Matheus Costa <41810688+MatheusHGCosta@users.noreply.github.com>
2021-09-02 12:24:57 -0300

    Add files via upload

---
+++ phpeu/conexao.php
@@ -0,0 +1,7 @@
+<?php
+define('HOST', "127.0.0.1");
+define('USUARIO', "root");
+define('SENHA', "");
+define('DB', "teste");
+$conexao = mysqli_connect($hostname=HOST, $username=USUARIO, $password=SENHA, $database=DB);
+?>
